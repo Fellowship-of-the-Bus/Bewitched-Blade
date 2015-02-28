769c36178952601c4c75550fd944e7d9f707b6cb
Abraham <A.Dubrisingh@gmail.com>
2015-02-28 15:25:35 -0500

    Blade Movement

---
+++ src/main/scala/bewitched_blade/game/blade.scala
@@ -0,0 +1,60 @@
+package com.github.fellowship_of_the_bus.bewitched_blade.game
+import scala.math.{atan2, toDegrees, sqrt}
+
+class GameObject (xc : Float, yc : Float) {
+    var x: Float = xc
+    var y: Float = yc
+    var ang: Float = 0
+    var xVel: Float = 0
+    var yVel: Float = 0
+    var angVel :Float = 0
+    // def id: Int
+
+    val hiltLen = 20
+    val hiltWidth = 20
+    val bladeLen = 60
+    val bladeWidth = 20
+    val accel = 1
+    val angAccel = 16 // 960 per sec
+
+    def move(mx : Float, my : Float) = {
+    	var xVec = mx - x
+    	var yVec = my - y
+    	var mTheta = atan2(yVec, xVec)
+    	mTheta = toDegrees(mTheta)
+
+    	var dTheta = ((mTheta - ang) + 360) % 360
+    	if (dTheta < 180) {
+    		angVel += angAccel
+    	} else {
+    		angVel -= angAccel
+    	}
+    	ang =  (ang + angVel) % 360
+
+    	var xAcc = xVec / sqrt((xVec * xVec) + (yVec * yVec)) * accel
+    	var yAcc = yVec / sqrt((xVec * xVec) + (yVec * yVec)) * accel
+    	xVel += xAcc.asInstanceOf[Float]
+    	yVel += yAcc.asInstanceOf[Float]
+    	x += xVel
+    	y += yVel
+    }
+
+    // def topLeftCoord() = (x-width/2, y-height/2)
+    // def bottomRightCoord() = (x+width/2, y+height/2)
+
+    // def collision(cand: GameObject) = if (active && cand.active) {
+    //     val (x1, y1) = topLeftCoord
+    //     val (x2, y2) = bottomRightCoord
+    //     val (cx1, cy1) = cand.topLeftCoord
+    //     val (cx2, cy2) = cand.bottomRightCoord
+        
+    //     def inRange(v: Float, min: Float, max: Float) = {
+    //       (v >= min) && (v <= max)
+    //     }
+
+    //     val xOver = inRange(cx1,x1,x2) || inRange(x1, cx1, cx2)
+    //     val yOver = inRange(cy1,y1,y2) || inRange(y1, cy1, cy2)
+
+    //     xOver && yOver
+    // } else false
+}
